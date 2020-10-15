6a251380cf34a4203d5574cb2ddbe4e2622c0f63
Bert Temme <btemme@hotmail.com>
2020-10-15 12:24:45 +0200

    add some more

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,7 @@
 using Dapper;
 using Npgsql;
 using System;
+using Wkx;
 
 namespace i3dm.export
 {
@@ -12,13 +13,15 @@ namespace i3dm.export
 
             Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
             {
+                string geom_column = "geom";
+                int epsg = 3857;
                 Console.WriteLine($"Exporting i3dm's from {o.Table}.");
                 Console.WriteLine($"Exporting tileset.json...");
                 SqlMapper.AddTypeHandler(new GeometryTypeHandler());
 
                 var conn = new NpgsqlConnection(o.ConnectionString);
                 // 1] Get boundingbox 3d for all positions in table in 3857 coordinates
-                var box3d = BoundingBoxRepository.GetBoundingBox3DForTable(conn, o.Table, "geom");
+                var box3d = BoundingBoxRepository.GetBoundingBox3DForTable(conn, o.Table, geom_column);
 
                 // 2] determine number of tiles in x- and y- direction
                 var xrange = (int)Math.Ceiling(box3d.ExtentX() / o.ExtentTile);
@@ -29,13 +32,19 @@ namespace i3dm.export
                 {
                     for (var y = 0; y < yrange; y++)
                     {
-                        //      4] calculate bounding box of tile
+                        // 4] calculate bounding box of tile
+                        var from = new Point(box3d.XMin + o.ExtentTile * x, box3d.YMin + o.ExtentTile * y);
+                        var to = new Point(box3d.XMin + o.ExtentTile * (x + 1), box3d.YMin + o.ExtentTile * (y + 1));
 
-                        //      5] get positions (in 3857), scale, rotations, properties for tile
+                        var hasFeatures = BoundingBoxRepository.HasFeaturesInBox(conn, o.Table, geom_column, from, to, epsg);
+                        if (hasFeatures)
+                        {
+                            //      5] get positions (in 3857), scale, rotations, properties for tile
 
-                        //       when there are positions in tile do:
+                            //       when there are positions in tile do:
 
-                        //          6] write tile_xy.i3dm
+                            //          6] write tile_xy.i3dm
+                        }
                     }
                 }
                 // 7] write tileset.json
