6d9754c6885c91475f5c6eb545c82c1730040fed
Bert Temme <btemme@hotmail.com>
2020-10-15 12:07:04 +0200

    adding getboundingbox

---
+++ src/BoundingBoxRepository.cs
@@ -0,0 +1,27 @@
+﻿using Npgsql;
+using System;
+
+namespace i3dm.export
+{
+    public static class BoundingBoxRepository
+    {
+        public static BoundingBox3D GetBoundingBox3DForTable(NpgsqlConnection conn, string geometry_table, string geometry_column)
+        {
+            conn.Open();
+            // todo: add conversion to 3857 in database?
+            var sql = $"SELECT st_xmin(geom1), st_ymin(geom1), st_zmin(geom1), st_xmax(geom1), st_ymax(geom1), st_zmax(geom1) FROM (select ST_3DExtent({geometry_column}) as geom1 from {geometry_table}";
+            var cmd = new NpgsqlCommand(sql, conn);
+            var reader = cmd.ExecuteReader();
+            reader.Read();
+            var xmin = reader.GetDouble(0);
+            var ymin = reader.GetDouble(1);
+            var zmin = reader.GetDouble(2);
+            var xmax = reader.GetDouble(3);
+            var ymax = reader.GetDouble(4);
+            var zmax = reader.GetDouble(5);
+            reader.Close();
+            conn.Close();
+            return new BoundingBox3D() { XMin = xmin, YMin = ymin, ZMin = zmin, XMax = xmax, YMax = ymax, ZMax = zmax };
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CommandLine;
 using Dapper;
+using Npgsql;
 using System;
 
 namespace i3dm.export
@@ -15,22 +16,28 @@ namespace i3dm.export
                 Console.WriteLine($"Exporting tileset.json...");
                 SqlMapper.AddTypeHandler(new GeometryTypeHandler());
 
-                // todo:
-
+                var conn = new NpgsqlConnection(o.ConnectionString);
                 // 1] Get boundingbox 3d for all positions in table in 3857 coordinates
+                var box3d = BoundingBoxRepository.GetBoundingBox3DForTable(conn, o.Table, "geom");
 
                 // 2] determine number of tiles in x- and y- direction
+                var xrange = (int)Math.Ceiling(box3d.ExtentX() / o.ExtentTile);
+                var yrange = (int)Math.Ceiling(box3d.ExtentY() / o.ExtentTile);
 
                 // 3] foreach tile in x- and y- direction do:
-                
-                //      4] calculate bounding box of tile
+                for (var x = 0; x < xrange; x++)
+                {
+                    for (var y = 0; y < yrange; y++)
+                    {
+                        //      4] calculate bounding box of tile
 
-                //      5] get positions (in 3857), scale, rotations, properties for tile
-                
-                //       when there are positions in tile do:
+                        //      5] get positions (in 3857), scale, rotations, properties for tile
 
-                //          6] write tile_xy.i3dm
+                        //       when there are positions in tile do:
 
+                        //          6] write tile_xy.i3dm
+                    }
+                }
                 // 7] write tileset.json
 
                 Console.WriteLine("");
