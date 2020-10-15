bb8d551d72cf3a7b7d2c9fd1023b3ab02d362b61
Bert Temme <btemme@hotmail.com>
2020-10-15 18:50:02 +0200

    fix conflict

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,13 @@
 ﻿using CommandLine;
 using Dapper;
+using i3dm.export.Tileset;
 using I3dm.Tile;
 using Npgsql;
 using ShellProgressBar;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Numerics;
 using Wkx;
 
@@ -18,6 +20,7 @@ namespace i3dm.export
 
             Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
             {
+                string tileFolder = "tiles";
                 string geom_column = "geom";
                 Console.WriteLine($"Exporting i3dm's from {o.Table}...");
                 SqlMapper.AddTypeHandler(new GeometryTypeHandler());
@@ -27,15 +30,19 @@ namespace i3dm.export
                 {
                     Directory.CreateDirectory(o.Output);
                 }
+                var tilefolder = $"{o.Output}{Path.DirectorySeparatorChar}{tileFolder}";
+                if (!Directory.Exists(tilefolder))
+                {
+                    Directory.CreateDirectory(tilefolder);
+                }
 
                 var conn = new NpgsqlConnection(o.ConnectionString);
 
-                // 1] Get boundingbox 3d for all positions in table in 3857 coordinates
-                var box3d = BoundingBoxRepository.GetBoundingBox3DForTable(conn, o.Table, geom_column);
+                var rootBounds = BoundingBoxRepository.GetBoundingBox3DForTable(conn, o.Table, geom_column);
+                var tiles = new List<TileInfo>();
 
-                // 2] determine number of tiles in x- and y- direction
-                var xrange = (int)Math.Ceiling(box3d.ExtentX() / o.ExtentTile);
-                var yrange = (int)Math.Ceiling(box3d.ExtentY() / o.ExtentTile);
+                var xrange = (int)Math.Ceiling(rootBounds.ExtentX() / o.ExtentTile);
+                var yrange = (int)Math.Ceiling(rootBounds.ExtentY() / o.ExtentTile);
 
                 var totalTicks = xrange * yrange;
                 var options = new ProgressBarOptions
@@ -45,20 +52,13 @@ namespace i3dm.export
                 };
                 var pbar = new ProgressBar(totalTicks, "Exporting i3dm tiles...", options);
 
-                // 3] foreach tile in x- and y- direction do:
                 for (var x = 0; x < xrange; x++)
                 {
                     for (var y = 0; y < yrange; y++)
                     {
-                        // 4] calculate bounding box of tile
-                        var from = new Point(box3d.XMin + o.ExtentTile * x, box3d.YMin + o.ExtentTile * y);
-                        var to = new Point(box3d.XMin + o.ExtentTile * (x + 1), box3d.YMin + o.ExtentTile * (y + 1));
-                        //  5] get positions (in 3857), scale, rotations, properties for tile
-                        // possible improvement: do not use wkx but convert to Vector3 immediatly 
+                        var from = new Point(rootBounds.XMin + o.ExtentTile * x, rootBounds.YMin + o.ExtentTile * y);
+                        var to = new Point(rootBounds.XMin + o.ExtentTile * (x + 1), rootBounds.YMin + o.ExtentTile * (y + 1));
                         var instances = BoundingBoxRepository.GetTileInstances(conn, o.Table, from, to);
-
-                        //Develop debug line
-                        //Console.WriteLine($"{instances.Count} intersected with tile {x}/{y}");
 
                         if (instances.Count > 0)
                         {
@@ -71,8 +71,14 @@ namespace i3dm.export
                             }
 
                             var i3dm = new I3dm.Tile.I3dm(positions, glbBytes);
-                            var i3dmFile = $"{o.Output}{Path.DirectorySeparatorChar}tile_{x}_{y}.i3dm";
+                            var i3dmFile = $"{o.Output}{Path.DirectorySeparatorChar}{tileFolder}{Path.DirectorySeparatorChar}tile_{x}_{y}.i3dm";
                             I3dmWriter.Write(i3dmFile, i3dm);
+
+                            tiles.Add(new TileInfo
+                            {
+                                Filename = $"{tileFolder}{Path.DirectorySeparatorChar}tile_{x}_{y}.i3dm",
+                                Bounds = new BoundingBox3D((float)from.X, (float)from.Y, 0, (float)to.X, (float)to.Y, 0)
+                            });
                         }
 
                         pbar.Tick();
@@ -80,8 +86,23 @@ namespace i3dm.export
                 }
                 // 7] todo: write tileset.json
                 Console.WriteLine();
+                Console.WriteLine("Writing tileset.json...");
+                WriteJson(o.Output, rootBounds, tiles, o.GeometricErrors);
                 Console.WriteLine("\nExport finished!");
             });
+        }
+
+        private static void WriteJson(string output, BoundingBox3D rootBounds, List<TileInfo> tiles, string geometricErrors)
+        {
+            var errors = geometricErrors.Split(',').Select(Double.Parse).ToList();
+            var tilesetJSON = TilesetGenerator.GetTileSetJson(rootBounds, tiles, errors);
+            var jsonFile = $"{output}{Path.DirectorySeparatorChar}tileset.json";
+
+            using (StreamWriter outputFile = new StreamWriter(jsonFile))
+            {
+                outputFile.WriteLine(tilesetJSON);
+                Console.WriteLine("tileset.json exported");
+            }
         }
     }
 }
